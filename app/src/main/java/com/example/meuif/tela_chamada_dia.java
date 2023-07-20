7f6fc11f3f4305843591173fbea93e2604c9a662
GabrielCM16 <gabrielcostademoraes13@gmail.com>
2023-07-19 22:44:52 -0300

    lista os nomes do firebase

--- app/src/main/java/com/example/meuif/tela_chamada_dia.java
+++ app/src/main/java/com/example/meuif/tela_chamada_dia.java
@@ -1,5 +1,6 @@
 package com.example.meuif;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -9,17 +10,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.meuif.ui.home.HomeFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.TimeZone;
 
 public class tela_chamada_dia extends AppCompatActivity {
@@ -29,7 +41,8 @@ public class tela_chamada_dia extends AppCompatActivity {
     private RecyclerView recycler;
     private nomesAdapter adapter;
     private ArrayList<nomes> itens;
-
+    private String turma;
+    private FirebaseFirestore db;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -45,6 +58,8 @@ public class tela_chamada_dia extends AppCompatActivity {
         diaAtual = diaAtual();
         saidaData.setText(diaAtual);
 
+        db = FirebaseFirestore.getInstance();
+
         ActionBar actionBar = getSupportActionBar();
 
         setTitle("Chamada Diaria de Classe");
@@ -56,20 +71,61 @@ public class tela_chamada_dia extends AppCompatActivity {
         actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_24); // Define o ícone de ação
         actionBar.setDisplayHomeAsUpEnabled(true); // Habilita o botão de navegação
 
-
-        setarrecylerView();
-
-
+        listarNomes();
 
 
     }
 
-    private void setarrecylerView(){
-        recycler = findViewById(R.id.recycler);
+    private String recuperarDados(String chave){
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        String aux = sharedPreferences.getString(chave, "");
+        return aux;
+    }
+
+    private void listarNomes(){
+        turma = recuperarDados("turma");
+        DocumentReference docRef = db.collection("ChamadaTurma").document(turma);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+
+                        List<String> stringArray = (List<String>) document.get("nomesSala");
+
+                        if (stringArray != null) {
+                            // Agora você tem a matriz de strings e pode fazer o que quiser com ela
+                            for (String stringValue : stringArray) {
+                                Log.d("TAG", "String: " + stringValue); // Exibir no logcat
+                            }
+                            setarrecylerView(stringArray);
+                        } else {
+                            Log.d("TAG", "Campo da matriz não encontrado no documento!");
+                        }
+
+                        Log.d("TAGBUSCANOMES", " achou o ducumento");
+                    } else {
+                        Log.d("TAGBUSCANOMES", "Documento de turma não encontrado");
+                    }
+                } else {
+                    Log.d("TAGBUSCANOMES", "Falhou em ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void setarrecylerView(List<String> listaNomes){
         itens = new ArrayList<nomes>();
-        itens.add(new nomes("Gabriel"));
-        itens.add(new nomes("Heloisa"));
-        itens.add(new nomes("Pedro"));
+
+        if (listaNomes != null) {
+            for (String nome : listaNomes) {
+                itens.add(new nomes(nome));
+            }
+        }
+
+        recycler = findViewById(R.id.recycler);
+
 
         adapter = new nomesAdapter(tela_chamada_dia.this , itens);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(tela_chamada_dia.this, LinearLayoutManager.VERTICAL, false);
