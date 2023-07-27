e7407c9563c2c7702319eb6df51964317c46d5e9
GabrielCM16 <gabrielcostademoraes13@gmail.com>
2023-07-27 16:22:10 -0300

    leitura de qr code

---
+++ app/src/main/java/com/example/meuif/CaptureAct.java
@@ -0,0 +1,7 @@
+package com.example.meuif;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity
+{
+}
--- app/src/main/java/com/example/meuif/Informacoes_pessoais.java
+++ app/src/main/java/com/example/meuif/Informacoes_pessoais.java
@@ -1,58 +1,41 @@
 package com.example.meuif;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.meuif.databinding.FragmentInformacoesPessoaisBinding;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Informacoes_pessoais#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Informacoes_pessoais extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private TextView teste;
     private FragmentInformacoesPessoaisBinding binding;
+    Button botao;
+    TextView textView;
+    final Informacoes_pessoais activity= this;
 
-
-
-    public Informacoes_pessoais() {
-        // Required empty public constructor
-    }
-
-
-    // TODO: Rename and change types and number of parameters
-    public static Informacoes_pessoais newInstance(String param1, String param2) {
-        Informacoes_pessoais fragment = new Informacoes_pessoais();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -64,10 +47,47 @@ public class Informacoes_pessoais extends Fragment {
 
         View root = binding.getRoot();
 
+
         teste = root.findViewById(R.id.testeaaa);
+        botao = root.findViewById(R.id.botao);
         teste.setText("ola");
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sCanCode();
+            }
+        });
 
         return root;
 
     }
+
+    private void sCanCode(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() !=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+            sCanCode();
+        }
+    });
+
 }
