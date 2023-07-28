983f3df8bd165cf10c4e1cfc8a2752017babc9ac
GabrielCM16 <gabrielcostademoraes13@gmail.com>
2023-07-27 21:58:53 -0300

    le o qr code, salva no firebase, e adiciona uma presenca

--- app/src/main/java/com/example/meuif/Informacoes_pessoais.java
+++ app/src/main/java/com/example/meuif/Informacoes_pessoais.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,16 +20,32 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.meuif.databinding.FragmentInformacoesPessoaisBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
 public class Informacoes_pessoais extends Fragment {
 
     private TextView teste;
+    private FirebaseFirestore db;
     private FragmentInformacoesPessoaisBinding binding;
-    Button botao;
+    private Button botao;
+    private long tempoValidade = 30000;
     TextView textView;
     final Informacoes_pessoais activity= this;
 
@@ -51,6 +69,7 @@ public class Informacoes_pessoais extends Fragment {
         teste = root.findViewById(R.id.testeaaa);
         botao = root.findViewById(R.id.botao);
         teste.setText("ola");
+        db = FirebaseFirestore.getInstance();
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,6 +79,17 @@ public class Informacoes_pessoais extends Fragment {
 
         return root;
 
+    }
+
+    private String diaAtual(){
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT-3"));
+
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+        int mes = calendar.get(Calendar.MONTH) + 1;
+        int ano = calendar.get(Calendar.YEAR);
+
+        String data = String.format("%02d%02d%d", dia, mes, ano);
+        return data;
     }
 
     private void sCanCode(){
@@ -75,19 +105,68 @@ public class Informacoes_pessoais extends Fragment {
     {
         if(result.getContents() !=null)
         {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-            builder.setTitle("Result");
-            builder.setMessage(result.getContents());
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
-            {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i)
-                {
-                    dialogInterface.dismiss();
+            if (result.getContents().contains("/")) {
+                String[] aux = result.getContents().split("/");
+
+                long valorCurrent = Long.parseLong(aux[1]);
+
+                if ( System.currentTimeMillis() - valorCurrent <= tempoValidade){
+                    String data = diaAtual();
+                    atualizarMatricula(aux[0], data);
                 }
-            }).show();
-            sCanCode();
+
+                sCanCode();
+            }
         }
     });
 
-}
+    private void atualizarMatricula(String matricula, String data){
+
+        Timestamp novoTimestamp = Timestamp.now();
+
+        // Envie os dados para o Firestore
+        DocumentReference docRef = db.collection("Usuarios").document("Alunos").collection(matricula)
+                .document("chamadaPessoal");
+
+            docRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    List<Timestamp> timestampsList;
+
+                    // Verifique se o documento existe e se o campo "timestamps" já foi criado
+                    if (task.getResult().exists() && task.getResult().contains(data)) {
+                        timestampsList = (List<Timestamp>) task.getResult().get(data);
+                    } else {
+                        // Se o documento não existir ou o campo da data não tiver sido criado, crie uma nova lista vazia
+                        timestampsList = new ArrayList<>();
+                        //adicionando uma presença a mais
+                        if (task.getResult().exists() && task.getResult().contains("presencas")){
+                            DocumentSnapshot document = task.getResult();
+                            String valorPresenca = document.getString("presencas");
+
+                            // Converta o valor atual para inteiro
+                            int valorAtualInt = Integer.parseInt(valorPresenca);
+
+                            // Atualize o campo com o novo valor convertido em string
+                            docRef.update("presencas", String.valueOf(valorAtualInt + 1))
+                                    .addOnSuccessListener(aVoid -> System.out.println("Campo incrementado com sucesso!"))
+                                    .addOnFailureListener(e -> System.out.println("Erro ao incrementar campo: " + e.getMessage()));
+
+                        } else {
+                        System.out.println("O documento não existe.");
+                        }
+                    }
+
+                    // Adicione o novo timestamp à lista
+                    timestampsList.add(novoTimestamp);
+
+                    // Use o método update() para atualizar o campo "timestamps" no documento
+                    docRef.update(data, timestampsList)
+                            .addOnSuccessListener(aVoid -> System.out.println("Timestamp adicionado com sucesso!"))
+                            .addOnFailureListener(e -> System.out.println("Erro ao adicionar timestamp: " + e.getMessage()));
+                } else {
+                    System.out.println("Erro ao obter o documento: " + task.getException().getMessage());
+                }
+            });
+        }
+    }
+
